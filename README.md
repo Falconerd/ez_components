99e1bd57c8a2894376d228a921a6b918d62c2470
Dylan Falconer <me@falconerd.com>
2023-08-13 20:39:18 +1000

    Remove test files, add shebang

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
 # \<grug/\> Grug Components
-> Simple reusable components for the grug brained developer
+> Simple reusable components for the grug brained developer.
+
+A preprocessor that uses HTML all the way down. No JSX, no objects, no extra languages; just HTML.
+
+## The simplest example
+```html
+// components/greeting.html
+<div>Hello, {name}</div>
+```
+```html
+// src/index.html
+<!DOCTYPE html>
+<html>
+
+  <head></head>
+
+  <body>
+    <greeting name="Alice" />
+  </body>
+
+</html>
+```
+```html
+// build/index.html
+<!DOCTYPE html>
+<html>
+
+  <head></head>
+
+  <body>
+    <div>Hello, Alice</div>
+  </body>
+
+</html>
+```
--- index.js
+++ index.js
@@ -1,73 +1,100 @@
+#!/usr/bin/env node
+
 const fs = require('fs');
+const path = require('path');
 const jsdom = require('jsdom');
 const pretty = require('pretty');
 const { JSDOM } = jsdom;
 
 console.time('Processing Time');
-// Read the index file
-fs.readFile('index.html', 'utf8', function (err, indexData) {
-  if (err) {
-    console.error('Error reading index file:', err);
-    return;
-  }
 
-  // Replace self-closing custom component tags with equivalent opening and closing tags
-  const modifiedIndexData = indexData.replace(/<([\w-]+)([^>]*)\/>/g, '<$1$2></$1>');
+const standardHtmlTags = ["a", "abbr", "address", "area", "article", "aside", "audio", "b", "base", "bdi", "bdo", "blockquote", "body", "br", "button", "canvas", "caption", "cite", "code", "col", "colgroup", "data", "datalist", "dd", "del", "details", "dfn", "dialog", "div", "dl", "dt", "em", "embed", "fieldset", "figcaption", "figure", "footer", "form", "h1", "h2", "h3", "h4", "h5", "h6", "head", "header", "hr", "html", "i", "iframe", "img", "input", "ins", "kbd", "label", "legend", "li", "link", "main", "map", "mark", "meta", "meter", "nav", "noscript", "object", "ol", "optgroup", "option", "output", "p", "param", "picture", "pre", "progress", "q", "rp", "rt", "ruby", "s", "samp", "script", "section", "select", "small", "source", "span", "strong", "style", "sub", "summary", "sup", "svg", "table", "tbody", "td", "template", "textarea", "tfoot", "th", "thead", "time", "title", "tr", "track", "u", "ul", "var", "video", "wbr"]
 
-  // Use JSDOM to parse the modified index file
-  const dom = new JSDOM(modifiedIndexData);
-  const document = dom.window.document;
-  const standardHtmlTags = [ "a", "abbr", "address", "area", "article", "aside", "audio", "b", "base", "bdi", "bdo", "blockquote", "body", "br", "button", "canvas", "caption", "cite", "code", "col", "colgroup", "data", "datalist", "dd", "del", "details", "dfn", "dialog", "div", "dl", "dt", "em", "embed", "fieldset", "figcaption", "figure", "footer", "form", "h1", "h2", "h3", "h4", "h5", "h6", "head", "header", "hr", "html", "i", "iframe", "img", "input", "ins", "kbd", "label", "legend", "li", "link", "main", "map", "mark", "meta", "meter", "nav", "noscript", "object", "ol", "optgroup", "option", "output", "p", "param", "picture", "pre", "progress", "q", "rp", "rt", "ruby", "s", "samp", "script", "section", "select", "small", "source", "span", "strong", "style", "sub", "summary", "sup", "svg", "table", "tbody", "td", "template", "textarea", "tfoot", "th", "thead", "time", "title", "tr", "track", "u", "ul", "var", "video", "wbr" ]
-
-  // Find all custom components
-  const components = Array.from(document.body.getElementsByTagName('*')).filter(element => !standardHtmlTags.includes(element.tagName.toLowerCase()));
-
-  // Read and replace each custom component
-  const processNextComponent = (index) => {
-    if (index >= components.length) {
-      // Format the HTML content using pretty
-      const formattedHtml = pretty(dom.serialize());
-      // Write the compiled HTML to a new file
-      fs.writeFile('compiled.html', formattedHtml, function (err) {
-        if (err) {
-          console.error('Error writing compiled file:', err);
-          return;
-        }
-        console.log('Compiled file written successfully.');
-        console.timeEnd('Processing Time');
-      });
+function processFile(filePath) {
+  fs.readFile(filePath, 'utf8', function (err, fileData) {
+    if (err) {
+      console.error('Error reading file:', filePath, err);
       return;
     }
 
-    const component = components[index];
-    const tagName = component.tagName.toLowerCase();
-    const childrenContent = component.innerHTML;
+    const modifiedData = fileData.replace(/<([\w-]+)([^>]*)\/>/g, '<$1$2></$1>');
 
-    // Read the corresponding component file from ./components/ directory
-    fs.readFile(`./components/${tagName}.html`, 'utf8', function (err, componentTemplate) {
-      if (err) {
-        console.error(`Error reading component file: ./components/${tagName}.html`, err);
-        processNextComponent(index + 1);
+    const dom = new JSDOM(modifiedData);
+    const document = dom.window.document;
+    const components = Array.from(document.body.getElementsByTagName('*')).filter(element => !standardHtmlTags.includes(element.tagName.toLowerCase()));
+
+    const processNextComponent = (index) => {
+      if (index >= components.length) {
+        const formattedHtml = pretty(dom.serialize());
+        const outputPath = path.join('build', filePath.substring(4));
+        const dir = path.dirname(outputPath);
+
+        if (!fs.existsSync(dir)) {
+          fs.mkdirSync(dir, { recursive: true });
+        }
+
+        fs.writeFile(outputPath, formattedHtml, function (err) {
+          if (err) {
+            console.error('Error writing compiled file:', outputPath, err);
+            return;
+          }
+          console.log('Compiled file written successfully:', outputPath);
+        });
         return;
       }
 
-      // Extract and replace attributes
-      for (const attr of component.attributes) {
-        const variablePattern = new RegExp('\\{' + attr.name + '}', 'g');
-        // console.log(`Replacing attribute: ${attr.name}, value: ${attr.value}, pattern: ${variablePattern}`); // Debugging line
-        componentTemplate = componentTemplate.replace(variablePattern, attr.value);
-      }
+      const component = components[index];
+      const tagName = component.tagName.toLowerCase();
+      const childrenContent = component.innerHTML;
 
-      // Replace {children} placeholder with the inner HTML content
-      componentTemplate = componentTemplate.replace('{children}', childrenContent);
+      fs.readFile(`./components/${tagName}.html`, 'utf8', function (err, componentTemplate) {
+        if (err) {
+          console.error(`Error reading component file: ./components/${tagName}.html`, err);
+          processNextComponent(index + 1);
+          return;
+        }
 
-      // Replace the custom component with the real component
-      component.outerHTML = componentTemplate;
+        for (const attr of component.attributes) {
+          const variablePattern = new RegExp('\\{' + attr.name + '}', 'g');
+          componentTemplate = componentTemplate.replace(variablePattern, attr.value);
+        }
 
-      processNextComponent(index + 1);
+        componentTemplate = componentTemplate.replace('{children}', childrenContent);
+        component.outerHTML = componentTemplate;
+
+        processNextComponent(index + 1);
+      });
+    };
+
+    processNextComponent(0);
+  });
+}
+
+function processDirectory(dir) {
+  fs.readdir(dir, function (err, files) {
+    if (err) {
+      console.error('Error reading directory:', dir, err);
+      return;
+    }
+
+    files.forEach(file => {
+      const filePath = path.join(dir, file);
+      fs.stat(filePath, function (err, stats) {
+        if (err) {
+          console.error('Error reading file stats:', filePath, err);
+          return;
+        }
+
+        if (stats.isDirectory()) {
+          processDirectory(filePath);
+        } else if (path.extname(filePath) === '.html') {
+          processFile(filePath);
+        }
+      });
     });
-  };
+  });
+}
 
-  processNextComponent(0);
-});
+processDirectory('src/');
 
+console.timeEnd('Processing Time');
