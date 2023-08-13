97f9417314462f59aa807c3ac43248a5dac26e20
Dylan Falconer <me@falconerd.com>
2023-08-13 16:28:39 +1000

    exclude html tags, allow self-closing tags

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const jsdom = require('jsdom');
 const pretty = require('pretty');
 const { JSDOM } = jsdom;
 
+console.time('Processing Time');
 // Read the index file
 fs.readFile('index.html', 'utf8', function (err, indexData) {
   if (err) {
@@ -10,12 +11,16 @@ fs.readFile('index.html', 'utf8', function (err, indexData) {
     return;
   }
 
-  // Use JSDOM to parse the index file
-  const dom = new JSDOM(indexData);
+  // Replace self-closing custom component tags with equivalent opening and closing tags
+  const modifiedIndexData = indexData.replace(/<(\w+)([^>]*)\/>/g, '<$1$2></$1>');
+
+  // Use JSDOM to parse the modified index file
+  const dom = new JSDOM(modifiedIndexData);
   const document = dom.window.document;
+  const standardHtmlTags = [ "a", "abbr", "address", "area", "article", "aside", "audio", "b", "base", "bdi", "bdo", "blockquote", "body", "br", "button", "canvas", "caption", "cite", "code", "col", "colgroup", "data", "datalist", "dd", "del", "details", "dfn", "dialog", "div", "dl", "dt", "em", "embed", "fieldset", "figcaption", "figure", "footer", "form", "h1", "h2", "h3", "h4", "h5", "h6", "head", "header", "hr", "html", "i", "iframe", "img", "input", "ins", "kbd", "label", "legend", "li", "link", "main", "map", "mark", "meta", "meter", "nav", "noscript", "object", "ol", "optgroup", "option", "output", "p", "param", "picture", "pre", "progress", "q", "rp", "rt", "ruby", "s", "samp", "script", "section", "select", "small", "source", "span", "strong", "style", "sub", "summary", "sup", "svg", "table", "tbody", "td", "template", "textarea", "tfoot", "th", "thead", "time", "title", "tr", "track", "u", "ul", "var", "video", "wbr" ]
 
   // Find all custom components
-  const components = Array.from(document.body.getElementsByTagName('*'));
+  const components = Array.from(document.body.getElementsByTagName('*')).filter(element => !standardHtmlTags.includes(element.tagName.toLowerCase()));
 
   // Read and replace each custom component
   const processNextComponent = (index) => {
@@ -29,6 +34,7 @@ fs.readFile('index.html', 'utf8', function (err, indexData) {
           return;
         }
         console.log('Compiled file written successfully.');
+        console.timeEnd('Processing Time');
       });
       return;
     }
@@ -47,7 +53,8 @@ fs.readFile('index.html', 'utf8', function (err, indexData) {
 
       // Extract and replace attributes
       for (const attr of component.attributes) {
-        const variablePattern = new RegExp('\\{' + attr.name + '\\}', 'g');
+        const variablePattern = new RegExp('\\{' + attr.name + '}', 'g');
+        // console.log(`Replacing attribute: ${attr.name}, value: ${attr.value}, pattern: ${variablePattern}`); // Debugging line
         componentTemplate = componentTemplate.replace(variablePattern, attr.value);
       }
 
